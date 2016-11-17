58f149fde2d17a3295fa7250d68d2a8b33072cf5
wangjj <wangjj886688@163.com>
2016-11-18 00:39:18 +0800

    58 同城

---
+++ week2/2_2_58_project/channel_extract.py
@@ -0,0 +1,85 @@
+#!python3
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import requests
+__author__ = 'wangjj'
+__mtime__ = '2016111721:31'
+start_url='http://hz.58.com/sale.shtml'
+url_host='http://hz.58.com'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'
+}
+def get_channel_urls(url):
+    global headers
+    wb_data=requests.get(url,headers=headers)
+    soup=BeautifulSoup(wb_data.text,'lxml')
+    links=soup.select('ul.ym-submnu > li > b > a')
+    global url_host
+    for link in links:
+        page_url=url_host+link.get('href')
+        print(page_url)
+get_channel_urls(start_url)
+
+channel_list='''
+http://hz.58.com/shouji/
+http://hz.58.com/shoujihao/
+http://hz.58.com/tongxunyw/
+http://hz.58.com/danche/
+http://hz.58.com/fzixingche/
+http://hz.58.com/diandongche/
+http://hz.58.com/sanlunche/
+http://hz.58.com/peijianzhuangbei/
+http://hz.58.com/diannao/
+http://hz.58.com/bijiben/
+http://hz.58.com/pbdn/
+http://hz.58.com/diannaopeijian/
+http://hz.58.com/zhoubianshebei/
+http://hz.58.com/shuma/
+http://hz.58.com/shumaxiangji/
+http://hz.58.com/mpsanmpsi/
+http://hz.58.com/youxiji/
+http://hz.58.com/jiadian/
+http://hz.58.com/dianshiji/
+http://hz.58.com/ershoukongtiao/
+http://hz.58.com/xiyiji/
+http://hz.58.com/bingxiang/
+http://hz.58.com/binggui/
+http://hz.58.com/chuang/
+http://hz.58.com/ershoujiaju/
+http://hz.58.com/bangongshebei/
+http://hz.58.com/diannaohaocai/
+http://hz.58.com/bangongjiaju/
+http://hz.58.com/ershoushebei/
+http://hz.58.com/yingyou/
+http://hz.58.com/yingeryongpin/
+http://hz.58.com/muyingweiyang/
+http://hz.58.com/muyingtongchuang/
+http://hz.58.com/yunfuyongpin/
+http://hz.58.com/fushi/
+http://hz.58.com/nanzhuang/
+http://hz.58.com/fsxiemao/
+http://hz.58.com/xiangbao/
+http://hz.58.com/meirong/
+http://hz.58.com/yishu/
+http://hz.58.com/shufahuihua/
+http://hz.58.com/zhubaoshipin/
+http://hz.58.com/yuqi/
+http://hz.58.com/tushu/
+http://hz.58.com/tushubook/
+http://hz.58.com/wenti/
+http://hz.58.com/yundongfushi/
+http://hz.58.com/jianshenqixie/
+http://hz.58.com/huju/
+http://hz.58.com/qiulei/
+http://hz.58.com/yueqi/
+http://hz.58.com/chengren/
+http://hz.58.com/nvyongpin/
+http://hz.58.com/qinglvqingqu/
+http://hz.58.com/qingquneiyi/
+http://hz.58.com/chengren/
+http://hz.58.com/xiaoyuan/
+http://hz.58.com/ershouqiugou/
+http://hz.58.com/tiaozao/
+http://hz.58.com/tiaozao/
+http://hz.58.com/tiaozao/
+'''
---
+++ week2/2_2_58_project/page_parsing.py
@@ -0,0 +1,50 @@
+#!python3
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import requests
+import time
+import pymongo
+__author__ = 'wangjj'
+__mtime__ = '2016111722:29'
+client = pymongo.MongoClient('localhost', 27017)
+ceshi = client['ceshi']
+url_list = ceshi['url_list']
+item_info = ceshi['item_info']
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'
+}
+
+
+def get_links_from(channel, pages, who_sells=0):
+    # http://hz.58.com/shouji/0/pn2/
+    global headers
+    list_view = '{}{}/pn{}'.format(channel, str(who_sells), str(pages))
+    wb_data = requests.get(list_view, headers=headers)
+    time.sleep(1)
+    soup = BeautifulSoup(wb_data.text, 'lxml')
+    if soup.find('td', 't'):
+        for link in soup.select('td.t a.t'):
+            item_link = (link.get('href')).split('&')[0]
+            url_list.insert_one({'url': item_link})
+            print(item_link)
+    else:
+        pass
+
+
+def get_item_info(url):
+    global headers
+    wb_data = requests.get(url, headers=headers)
+    soup = BeautifulSoup(wb_data.text, 'lxml')
+    sold_out = soup.find('span', 'soldout_btn')
+    if sold_out:
+        pass
+    else:
+        title = soup.select('div.box_left_top > h1')[0].get_text()
+        category = (soup.select('div.breadCrumb')[0]).stripped_strings
+        price = soup.select('span.price_now > i')[0].get_text()
+        place = soup.select('div.palce_li > span > i')[0].get_text()
+        item_info.insert_one(
+            {'category': list(category), 'title': title, 'price': price, 'place': place})
+        print({'category': list(category), 'title': title,
+               'price': price, 'place': place})
+
